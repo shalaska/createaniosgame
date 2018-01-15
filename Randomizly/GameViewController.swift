8b5aaf4dd5664b5c601d6f5770f3618f9fc484d7
shalaska <yiazamat@hotmail.com>
2018-01-15 13:25:46 +0100

    Actual end of checking input chapter

--- Randomizly/GameViewController.swift
+++ Randomizly/GameViewController.swift
@@ -5,7 +5,7 @@ class GameViewController: UIViewController {
   @IBOutlet weak var inputField: UITextField!
   
   @IBAction func guess(_ sender: Any) {
-    if let input = self.inputField.text, input != "" {
+    if let input = self.inputField.text, let number = Int(input) {
       print("Input: \(input)")
     } else {
       print("No input")
