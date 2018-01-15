4046eb2d8ae0b783cba2432907d11fdf4fcb107e
shalaska <yiazamat@hotmail.com>
2018-01-15 13:23:50 +0100

    End of checking input chapter

--- Randomizly/GameViewController.swift
+++ Randomizly/GameViewController.swift
@@ -1,35 +1,15 @@
-//
-//  GameViewController.swift
-//  Randomizly
-//
-//  Created by Joseph on 15.01.18.
-//  Copyright © 2018 User. All rights reserved.
-//
-
 import UIKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var inputField: UITextField!
+  
+  @IBAction func guess(_ sender: Any) {
+    if let input = self.inputField.text, input != "" {
+      print("Input: \(input)")
+    } else {
+      print("No input")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
 }
